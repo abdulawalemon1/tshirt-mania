2ecfdb6fec6b8ae4369eaf39daec8a5dc709bd98
abdulawalemon1 <emondx4@gmail.com>
2022-04-01 02:44:45 +0600

    add to cart added & delte button added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useTShirts from '../../hooks/useTShirts';
 import Cart from '../Cart/Cart';
 import Tshirt from '../Tshirt/Tshirt';
@@ -6,18 +6,42 @@ import './Home.css';
 
 const Home = () => {
     const [tShirts, setTShirts] = useTShirts();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = (selectedItem) => {
+        const exists = cart.find(tShirt => tShirt._id === selectedItem._id);
+        if (!exists) {
+            const newCart = [...cart, selectedItem];
+            setCart(newCart);
+        } else {
+            alert('Items already added!!!');
+        }
+
+
+
+    }
+
+    const handleRemoveFromCart = (selectedItem) => {
+        const rest = cart.filter(tShirt => tShirt._id !== selectedItem._id);
+        setCart(rest);
+    }
     return (
         <div className='home-container'>
             <div className='tshirt-container'>
                 {
                     tShirts.map(tShirt => <Tshirt
-                        key={tShirt.id}
+                        key={tShirt._id}
                         tShirt={tShirt}
+                        handleAddToCart={handleAddToCart}
                     ></Tshirt>)
                 }
             </div>
             <div className='cart-container'>
-                <Cart></Cart>
+                <Cart
+                    key={cart.id}
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
             </div>
         </div>
     );
